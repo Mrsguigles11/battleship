0061ddee66f22b9be23ec2e168840b722f971ca6
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-07-16 11:52:41 +0100

    include test for updating game status

--- src/classes.test.js
+++ src/classes.test.js
@@ -24,21 +24,21 @@ test("place ship on gameboard", () => {
     expect(newGameboard.ships[1].coordinates.has(JSON.stringify([1, 1]))).toBe(true);
     expect(newGameboard.ships[1].coordinates.has(JSON.stringify([5, 1]))).toBe(true);
     newGameboard.placeShip(5, [1, 1], "vertical");
-    newGameboard.placeShip(5, [1, 1], "vertical");
     expect(newGameboard.ships.length).toBe(2);
 })
 
 test("receive attack", () => {
     const newGameboard = new Gameboard();
     newGameboard.placeShip(3, [1, 1], "horizontal");
+    newGameboard.placeShip(1, [4, 4], "horizontal");
     newGameboard.receiveAttack([1, 2]);
     expect(newGameboard.ships[0].hits).toBe(1);
     newGameboard.receiveAttack([1, 3]);
     expect(newGameboard.hits.size).toEqual(2);
-    newGameboard.receiveAttack([1, 3]);
-    expect(newGameboard.gamestatus).toMatch("you already moved there!");
-    newGameboard.receiveAttack([1, 1])
-    expect(newGameboard.gamestatus).toMatch("game over");
+    newGameboard.receiveAttack([1, 1]);
+    expect(newGameboard.gameStatus).toMatch("ships sunk 1");
+    newGameboard.receiveAttack([4, 4])
+    expect(newGameboard.gameStatus).toMatch("game over");
 })
 
 test("place random ship", () => {
@@ -48,3 +48,10 @@ test("place random ship", () => {
     newGameboard.placeRandomShip(3);
     expect(newGameboard.ships.length).toBe(2);
 })
+
+test("receive random attack", () => {
+    const newGameboard = new Gameboard();
+    newGameboard.receiveRandomAttack();
+    expect(newGameboard.misses.size).toBe(1);
+})
+
--- src/rendering.js
+++ src/rendering.js
@@ -52,17 +52,13 @@ function renderGameboards(player, computer) {
         player.gameboard.receiveRandomAttack();
         renderGameboards(player, computer);
         gameStatus.textContent = player.gameboard.gameStatus === "game over" ? player.gameboard.gameStatus : computer.gameboard.gameStatus;
-        // gameStatus.textContent = computer.gameboard.gameStatus;
-
-        // if (computer.gameboard.gameStatus === "game over" || player.gameboard.gameStatus === "game over") {
-        // gameStatus.textContent = computer.gameboard.gameStatus;
-        // }
-        console.log(computer.gameboard)
       });
       }
       computerGameboard.appendChild(rowDiv);
     }
 
 }
+
+
 
 export { renderGameboards };

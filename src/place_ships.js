3ec980eda0345fbd265679aaa36521acdd37af16
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-07-20 19:15:46 +0100

    add a click event listener to addHorizontalHover so that it will place ship on gameboard

--- src/place_ships.js
+++ src/place_ships.js
@@ -1,29 +1,36 @@
+import { player } from "./index.js";
+import { renderPlayerGameboard } from "./rendering";
+
 
 function addHorizontalHover(length) {
   const playerGameboardRows =
     document.querySelector("#player_gameboard").children;
 
-  for (const row of playerGameboardRows) {
-    const rowSquares = row.children;
+  for (let row = 0; row < 10; row++) {
+    const rowSquares = playerGameboardRows[row].children;
 
-    for (let i = 0; i <= 10 - length; i++) {
-      const cell = rowSquares[i];
+    for (let col = 0; col <= 10 - length; col++) {
+      const cell = rowSquares[col];
       cell.addEventListener("mouseover", () => {
         cell.classList.add("square_hovered");
         for (let x = 1; x < length; x++) {
-          const targetCell = rowSquares[i + x];
+          const targetCell = rowSquares[col + x];
           targetCell.classList.add("square_hovered");
         }
       });
       cell.addEventListener("mouseout", () => {
         cell.classList.remove("square_hovered");
         for (let x = 1; x < length; x++) {
-          rowSquares[i + x].classList.remove("square_hovered");
+          rowSquares[col + x].classList.remove("square_hovered");
         }
       });
+      cell.addEventListener('click', () => {
+        player.gameboard.placeShip(length, [row + 1, col + 1], "horizontal");
+        renderPlayerGameboard(player);
+      })
     }
-    for (let i = 10 - length + 1; i < 10; i++) {
-        let cell = rowSquares[i];
+    for (let col = 10 - length + 1; col < 10; col++) {
+        let cell = rowSquares[col];
         cell.addEventListener("mouseover", () => {
             for (let x = 10 - length; x < 10; x++) {
                 rowSquares[x].classList.add("square_hovered");
@@ -34,6 +41,10 @@ function addHorizontalHover(length) {
                 rowSquares[x].classList.remove("square_hovered");
             }
         })
+        cell.addEventListener('click', () => {
+        player.gameboard.placeShip(length, [row + 1, 10 - length + 1], "horizontal");
+        renderPlayerGameboard(player);
+      })
 
     }
   }
